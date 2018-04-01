561f015f12a290cd5a9beda7e36ba2deced563a8
anseki <AnSeki.aff@gmail.com>
2018-04-01 11:38:16 +0900

    Apply template

--- test/default.test.js
+++ test/default.test.js
@@ -22,46 +22,10 @@ function newFile(content, path) {
   });
 }
 
-describe('implements a basic flow as buffer based plugin', () => {
+describe('implements a basic flow as Buffer based plugin', () => {
   const OPTS = {p1: 'v1', p2: 'v2'};
 
-  it('should skip process if contents is null', done => {
-    resetAll();
-    const pluginStream = plugin(OPTS),
-      passedFile = new File();
-    expect(passedFile.isNull()).to.be.true;
-    expect(passedFile.isStream()).to.be.false;
-    expect(passedFile.isBuffer()).to.be.false;
-
-    pluginStream.write(passedFile);
-    pluginStream.once('data', file => {
-      expect(file.isNull()).to.be.true;
-      expect(file.isStream()).to.be.false;
-      expect(file.isBuffer()).to.be.false;
-      expect(htmlclean.notCalled).to.be.true;
-
-      done();
-    });
-  });
-
-  it('should throw an error if contents is a Stream', () => {
-    resetAll();
-    const stream = require('stream'),
-      es = require('event-stream'),
-      pluginStream = plugin(OPTS),
-      passedFile = new File({
-        contents: new stream.Readable({objectMode: true})
-          .wrap(es.readArray(['stream', 'with', 'those', 'contents']))
-      });
-    expect(passedFile.isNull()).to.be.false;
-    expect(passedFile.isStream()).to.be.true;
-    expect(passedFile.isBuffer()).to.be.false;
-
-    expect(() => { pluginStream.write(passedFile); }).to.throw('Streaming not supported');
-    expect(htmlclean.notCalled).to.be.true;
-  });
-
-  it('should accept contents if it is a Buffer', done => {
+  it('should accept contents from Buffer', done => {
     resetAll();
     const pluginStream = plugin(OPTS),
       passedFile = newFile('content');
@@ -76,6 +40,40 @@ describe('implements a basic flow as buffer based plugin', () => {
       expect(file.isBuffer()).to.be.true;
       expect(htmlclean.calledOnceWithExactly('content', OPTS)).to.be.true;
       expect(file.contents.toString()).to.equal('content<htmlclean>');
+
+      done();
+    });
+  });
+
+  it('should throw an error if a Stream is input', () => {
+    resetAll();
+    const pluginStream = plugin(OPTS),
+      passedFile = new File({
+        contents: new (require('stream')).Readable({objectMode: true})
+          .wrap(require('event-stream').readArray(['stream', 'with', 'those', 'contents']))
+      });
+    expect(passedFile.isNull()).to.be.false;
+    expect(passedFile.isStream()).to.be.true;
+    expect(passedFile.isBuffer()).to.be.false;
+
+    expect(() => { pluginStream.write(passedFile); }).to.throw('Streaming not supported');
+    expect(htmlclean.notCalled).to.be.true;
+  });
+
+  it('should skip process if a null is input', done => {
+    resetAll();
+    const pluginStream = plugin(OPTS),
+      passedFile = new File();
+    expect(passedFile.isNull()).to.be.true;
+    expect(passedFile.isStream()).to.be.false;
+    expect(passedFile.isBuffer()).to.be.false;
+
+    pluginStream.write(passedFile);
+    pluginStream.once('data', file => {
+      expect(file.isNull()).to.be.true;
+      expect(file.isStream()).to.be.false;
+      expect(file.isBuffer()).to.be.false;
+      expect(htmlclean.notCalled).to.be.true;
 
       done();
     });
